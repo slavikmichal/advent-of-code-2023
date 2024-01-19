cf2140ff9d650ea46708889c639af5ee3b0e9921
Slavík Michal <michal.slavik@ysoft.com>
2024-01-19 12:22:39 +0100

    🎅day 7 part one
    for good measure

---
+++ day 7/7a.scala
@@ -0,0 +1,66 @@
+import scala.io.Source
+import scala.collection.mutable.Map
+import scala.collection.mutable.ListBuffer
+import math.Ordered.orderingToOrdered
+
+object Hello {
+
+    object Rules {
+        val cardValues = List("2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A")
+    }
+
+    case class Hand(cards: String, value: Long, kinds: List[List[String]] = List.empty[List[String]])
+
+    def getFile(name: String): List[String] = {
+        val source = Source.fromFile(name)
+        val lines = source.getLines().toList
+        source.close()
+        lines
+    }
+
+    def mapValues(lines: List[String]): List[Hand] = {
+        var map = ListBuffer.empty[Hand]
+        for (line <- lines) {
+            val parsed: Array[String] = line.split("\\s+", 2)
+            val kinds = nOfAKind(parsed(0))
+            map += Hand(parsed(0), parsed(1).toLong, kinds)
+        }
+        map.toList
+    }
+
+    def nOfAKind(hand: String): List[List[String]] = {
+        var ranks = ListBuffer.empty[List[String]]
+        for (card <- Rules.cardValues) {
+            ranks += card.r.findAllIn(hand).toList
+        }
+        return ranks.filterNot(l => l.isEmpty).toList
+    }
+
+    def getCardRank(card: Char): Int = {
+        Rules.cardValues.indexOf(card.toString)
+    }
+
+    def getHandRank(hand: List[List[String]]): Int = {
+        hand.map(e => e.size).max match
+            case 5 => 7
+            case 4 => 6
+            case 3 => hand.map(e => e.size).sorted.reverse(1) match
+                case 2 => 5
+                case 1 => 4
+            case 2 => hand.map(e => e.size).sorted.reverse(1) match
+                case 2 => 3
+                case 1 => 2
+            case 1 => 1
+    }
+
+    def main(args: Array[String]) = {
+        val lines = getFile("/Users/slavik/Documents/Fun/advent-of-code-2023/day 7/7.txt")
+        val hands = mapValues(lines)
+        var result: Long = 0
+        val sorted = hands.sortBy(hand => (getHandRank(hand.kinds), getCardRank(hand.cards(0)), getCardRank(hand.cards(1)), getCardRank(hand.cards(2)), getCardRank(hand.cards(3)), getCardRank(hand.cards(4))))
+        for (index <- 0 to sorted.size - 1) {
+            result += (index.toLong + 1) * sorted(index).value
+        }
+        println(result)
+    }
+}
